6e366c0600735bb72662d8fe6bc48e355ee345a7
Christoph Dorn <christoph@christophdorn.com>
2017-03-20 22:35:05 -0700

    wip

--- _#_org.bashorigin_#_s1.sh
+++ _#_org.bashorigin_#_s1.sh
@@ -10,6 +10,8 @@ fi
 function EXPORTS_run {
     BO_run_node --eval '
         const PROCESS = require("$__DIRNAME__/lib/firefox");
-        PROCESS.run(JSON.parse(process.argv[1])).catch(console.error);
+        PROCESS.run(JSON.parse(process.argv[1])).catch(function (err) {
+            console.error("[it.pinf.org.mozilla.firefox] ERROR:", err.stack);
+        });
     ' "$1"
 }

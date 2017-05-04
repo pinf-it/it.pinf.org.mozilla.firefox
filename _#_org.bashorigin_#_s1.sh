a68ea02a91162539b0d0d54b3a7cc6eee182bb4f
Christoph Dorn <christoph@christophdorn.com>
2017-05-03 21:08:39 -0700

    faster node

--- _#_org.bashorigin_#_s1.sh
+++ _#_org.bashorigin_#_s1.sh
@@ -8,7 +8,7 @@ fi
 
 
 function EXPORTS_run {
-    BO_run_node --eval '
+    BO_run_recent_node --eval '
         const PROCESS = require("$__DIRNAME__/lib/firefox");
         PROCESS.run(JSON.parse(process.argv[1])).catch(function (err) {
             console.error("[it.pinf.org.mozilla.firefox] ERROR:", err.stack);

f1f994e855940fd147c5b83281bb6d80a4256ac5
Christoph Dorn <christoph@christophdorn.com>
2018-09-26 19:49:52 -0700

    wip

--- _#_org.bashorigin_#_s1.sh
+++ _#_org.bashorigin_#_s1.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash.origin.script
 
-if [ ! -e "$__DIRNAME__/node_modules" ]; then
-    pushd "$__DIRNAME__" > /dev/null
-        BO_run_npm install
-    popd > /dev/null
-fi
-
-
 function EXPORTS_run {
     BO_run_recent_node --eval '
         const PROCESS = require("$__DIRNAME__/lib/firefox");
